04699e5be47219de0b2e9cdcb604640a2debf151
samajaloleru <samsonajaloleru@gmail.com>
2022-12-20 17:13:18 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
@@ -14,13 +15,8 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style media="screen">
+    /* @import "assets/css/fontawesomepro.css"; */
+    @import "assets/css/tachyons.css";
+    @import "assets/css/custom.css";
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,105 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="vh-100 h-100 dt w-100 tc">
+    <div class="dtc v-mid">
+      <span class="z-max top-0 right-2 absolute">
+        <img width="150px" class="glow" src="../assets/img/logo.png"/>
+      </span>
+      <div class="fl w-100 tc">
+        <div class="cf w7-l tc center">
+          <div id="clockdiv" class="relative flex flex-row heartbeat items-center justify-center">
+            <div class="w-auto pa3 hover-light-pink br4">
+              <div class="fl w-100 f-subheadline-l f1-m f2 b days"></div>
+              <div class="fl w-100 f3 b">Days</div>
+            </div> 
+            <div class="w-auto">
+              <div class="glow fl w-100">:</div>
+            </div> 
+            <div class="w-auto pa3 hover-light-pink br4">
+              <div class="fl w-100 f-subheadline-l f1-m f2 b hours">00</div>
+              <div class="fl w-100 f3 b">Hours</div>
+            </div>
+            <div class="w-auto">
+              <div class="fl w-100">:</div>
+            </div>
+            <div class="w-auto pa3 hover-light-pink br4">
+              <div class="fl w-100 f-subheadline-l f1-m f2 b minutes">00</div>
+              <div class="fl w-100 f3 b">Minutes</div>
+            </div>
+            <div class="w-auto">
+              <div class="fl w-100">:</div>
+            </div>
+            <div class="w-auto pa3 hover-light-pink br4">
+              <div class="fl w-100 f-subheadline-l f1-m f2 b seconds">00</div>
+              <div class="fl w-100 f3 b">Seconds</div>
+            </div>
+          </div>
+        </div>
+        <div class="cf tracking-in-expand-fwd w-50-l f6 measure center hover-bg-black-30 hover-light-pink bg-light-pink navy b pv3 ph2 br2 mt4 tc">
+          May this new year bring us a fantastic January, a brilliant February, a calm March, an anxiety-free April, a phenomenal May, and joy that lasts from June to November, with a happy December to cap it off.
+        </div>
+      </div>
+    </div>
   </div>
+    
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data(){
+    return{
+      deadline: new Date("dec 31, 2022 23:59:59"), daysSpan:'', hoursSpan: '', minutesSpan: '', secondsSpan: '', timeinterval: ''
+    }
+  },
+  mounted(){
+    this.getCountdown()
+  },
+  methods: {
+    getCountdown(){
+      console.log("deadline", this.deadline);
+      
+      this.initializeClock('clockdiv', this.deadline);
+    },
+    getTimeRemaining() {
+      const total = Date.parse(this.deadline) - Date.parse(new Date());
+      const seconds = Math.floor((total / 1000) % 60);
+      const minutes = Math.floor((total / 1000 / 60) % 60);
+      const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+      const days = Math.floor(total / (1000 * 60 * 60 * 24));
+      
+      return {
+        total,
+        days,
+        hours,
+        minutes,
+        seconds
+      };
+    },
+
+    initializeClock(id,) {
+      const clock = document.getElementById(id);
+      this.daysSpan = clock.querySelector('.days');
+      this.hoursSpan = clock.querySelector('.hours');
+      this.minutesSpan = clock.querySelector('.minutes');
+      this.secondsSpan = clock.querySelector('.seconds');
+      
+      this.updateClock();
+      
+      this.timeinterval = setInterval(this.updateClock, 1000);
+    },
+    updateClock() {
+        const t = this.getTimeRemaining(this.deadline);
+
+        this.daysSpan.innerHTML = t.days;
+        this.hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+        this.minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+        this.secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+        if (t.total <= 0) {
+          clearInterval(this.timeinterval);
+        }
+      }
+
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
